f59bdebd829b244e236a8f2c9044b179ae9c3e32
Thamires <Thamires@192.168.1.28>
2017-06-17 17:26:05 -0300

    Lendo arquivo de informações de pokemons, imprimindo lista, pokemon capturado e informações pokedex.

---
+++ TrabalhoIA2/src/trabalhoia2/Pokemon.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhoia2;
+
+/**
+ *
+ * @author Thamires
+ */
+public class Pokemon {
+    
+    private int identificador;
+    private String nome, descricao, tipo, habilidade;
+
+    public Pokemon(){}
+    
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getHabilidade() {
+        return habilidade;
+    }
+
+    public void setHabilidade(String habilidade) {
+        this.habilidade = habilidade;
+    }
+    
+    
+    public String toString() {
+         return identificador + "\n" + nome + "\n" + descricao + "\n" + tipo + "\n" + habilidade + "\n";
+    }
+}
+
