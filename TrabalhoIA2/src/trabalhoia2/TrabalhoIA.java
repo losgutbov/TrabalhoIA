65bb4251cd3c11b91366be0575d6f64ac2bf4854
Thamires <Thamires@192.168.1.28>
2017-07-03 22:13:41 -0300

    incluindo comentarios

--- TrabalhoIA2/src/trabalhoia2/TrabalhoIA.java
+++ TrabalhoIA2/src/trabalhoia2/TrabalhoIA.java
@@ -11,21 +11,22 @@ import java.io.IOException;
  *
  * @author Augusto
  */
+
+/*---------------CLASSE MAIN DO PROJETO--------*/
 public class TrabalhoIA {
 
-    /**
-     * @param args the command line arguments
-     */
+    //-------------------------MÉTODO MAIN---------------------------------//
+    /* MÉTODO UTILIZADO PARA INSTANCIAR OS DEMAIS OBJETOS DO PROJETO E CHAMAR SEUS MÉTODOS PRINCIPAIS*/
     public static void main(String[] args) throws IOException {
         RegrasFuncionamento regra = new RegrasFuncionamento();
         regra.lerMatrizTerreno();
         regra.sortearTudo();
         regra.lerInformacoesPokemon();
-        Interface inter = new Interface(730, 1370, regra); //Instancia objeto do tipo interface que carrega a interface gráfica do jogo, com os controles.
-        AgentProlog  agente = new AgentProlog(inter);
+        Interface inter = new Interface(730, 1370, regra); //INSTANCIA O OBJETO DO TIPO INTERFACE QUE CARREGA A INTERFACE GRÁFICA DO JOGO, COM OS CONTROLES.
+        AgentProlog  agente = new AgentProlog(inter); //INSTANCIA O OBJETO DO TIPO AGENTE PROLOG QUE FAZ A COMUNICAÇÃO ENTRE O PROLOG E O JAVA.
         inter.geraInterface();
         agente.executarAgente();
-        //System.out.println(regra.getListaPokemons().get(0).stringToProlog());
-        
+        //System.out.println(regra.getListaPokemons().get(0).stringToProlog());   
     }
+    //-------------------------FIM DO MÉTODO MAIN---------------------------------//
 }
